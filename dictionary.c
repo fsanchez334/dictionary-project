a650c116e6db1da7cb352c9107d07a198216c5c6
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-03 13:33:30 -0400

    I tidied up the spacing among the code and removed unneccesary lines

--- dictionary.c
+++ dictionary.c
@@ -2,24 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Node{
-    struct Node *next;
-    char *word;
-    char *definition;
-};
-
-struct List {
-    struct Node *head;
-
-};
-
-
 char *maker_function(int length){
      char *holder = (char *) malloc(length);
      return holder;
-
 }
-
 char *delimeterFunction(char *define){
     char dash = '-';
     int string_len = strlen(define);
@@ -30,13 +16,8 @@ char *delimeterFunction(char *define){
     }
     return define;
 }
-
-
 int main(){
     printf("%s\n", "We are going to make a mini dictionary");
-
-
-
     printf("%s\n", "How many words will you be entering?");
     int amount = 0;
 
@@ -45,6 +26,7 @@ int main(){
 
     char word[100];
     char *list_of_words[amount];
+    char *word_definitions[amount];
     int stop = 0;
     int counter = 0;
 
@@ -54,30 +36,34 @@ int main(){
     {
         printf("%s\n", "Enter the word: ");
         scanf("%s", word);
-        printf("-------------------------------");
 
         int word_length = strlen(word);
         char *face = maker_function(word_length);
         strcpy(face, word);
-        printf("%s\n", face);
 
         list_of_words[counter] = face;
 
         char define[500];
         printf("%s\n", "Please provide for the definition you provided");
+        printf("%s\n", "In between each word, please include a dash");
         scanf("%s", define);
 
+        char *response = delimeterFunction(define);
         int len_define = strlen(define);
         char *definer = maker_function(word_length);
 
         strcpy(definer, define);
-        printf("%s\n", definer);
+        word_definitions[counter] = definer;
 
         ++counter;
         ++stop;
     }
+    printf("%s\n", "Your dictionary: ");
+    int tracker = 1;
     for(int j = 0; j < amount; j++){
-        printf("%s\n", list_of_words[j]);
+        printf("%d%s%s%s", tracker, ". ", list_of_words[j], " - ");
+        printf("%s\n", word_definitions[j]);
+        ++tracker;
     }
 
     return 0;
