bc26b184fd9a9b3540fb80c1858f8dd3bee7bff0
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-07 23:24:00 -0400

    Made some small changes, and made a second program that is similar to the first version

--- dictionary-version-two/dictionary-two.c
+++ dictionary-version-two/dictionary-two.c
@@ -2,4 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        fprintf(stderr, "<program-name> <number-of-words> <word-one> <word -n> ...");
+    }
+
+    printf("%s\n", "Please enter the number of words you will want to input, followed by the words");
+    char *holder[argc];
+
+    for(int 
+}
 
