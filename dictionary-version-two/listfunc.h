3baa989337d50c69194eb09088bee4a00dd9921c
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-16 01:33:25 -0400

    Added a header file that has the method of analyzing whether the user wants to continue using our dictionary, and a method that pushes words onto the dictionary

---
+++ dictionary-version-two/listfunc.h
@@ -0,0 +1,42 @@
+#ifndef _LISTFUNC_H_
+#define _LISTFUNC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct List{
+  struct Node *head;
+};
+
+struct Node {
+  char word[10];
+  char definition[100];
+  struct Node *next;
+};
+int determineResponse(char *input){
+    int result = strcmp("Yes", input);
+    if(result == 0){
+        return 0;
+    }
+    return 1;
+}
+ void push(struct List *list, struct Node *container){
+    struct Node *beginner = list -> head;
+    if(beginner == NULL){
+        list -> head = (struct Node *) malloc(sizeof(struct Node));
+        strcpy((list -> head) -> word, container -> word);
+        strcpy((list -> head) -> definition, container -> definition);
+        (list -> head) -> next = NULL;
+        return;
+    }
+    while(beginner -> next){
+        beginner = beginner -> next;
+    }
+    beginner -> next = (struct Node *) malloc(sizeof(struct Node));
+    strcpy((beginner -> next) -> word, container -> word);
+    strcpy((beginner -> next) -> definition, container -> definition);
+    (beginner -> next) -> next = NULL;
+}
+#endif /* #ifndef _CHARFUNC_H_ */
+
