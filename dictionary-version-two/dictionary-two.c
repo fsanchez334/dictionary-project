4bc24c0acdd55140d0fc0e784e79757f297e8965
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-06-16 01:57:07 -0400

    Made some small changes to typos, and added a variable that tracks the number of entries

--- dictionary-version-two/dictionary-two.c
+++ dictionary-version-two/dictionary-two.c
@@ -1,52 +1,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "listfunc`.h"
+#include "listfunc.h"
 
 
 int main()
 {
     printf("Hello World\n");
-    
+
     int go_flag = 0;
     struct List *dictionary = (struct List *) malloc(sizeof(struct List));
-    
-    while(go == 0){
-    
-	char input_one[10];
-	char definition_one[100];
-	char user[10];
 
-        printf("%s\n", "Please enter a word");
-        scanf("%s", input_one);
-    
-        printf("%s\n", "Please enter a definition");
-        scanf("%s", definition_one);
+    while(go_flag == 0){
 
-    
+	     char input_one[10];
+	     char definition_one[100];
+	     char user[10];
+
+       printf("%s\n", "Please enter a word");
+       scanf("%s", input_one);
+
+       printf("%s\n", "Please enter a definition");
+       scanf("%s", definition_one);
+
         struct Node *beginner = (struct Node *) malloc(sizeof(struct Node));
-        strcpy(beginner -> word, input one);
-	strcpy(beginner -> definition, definition_one);
+        strcpy(beginner -> word, input_one);
+	      strcpy(beginner -> definition, definition_one);
 
-	push(dictionary, beginner);
-	
-	printf("%s\n", "Would you like to continue using our dictionary? Yes/No");
-	scanf("%s", response);
+	      push(dictionary, beginner);
 
-	int compare = determineResponse(user);
-	if(compare == 1){
-	    go = 1;
-	}
+	      printf("%s\n", "Would you like to continue using our dictionary? Yes/No");
+	      scanf("%s", user);
+
+	      int compare = determineResponse(user);
+	       if(compare == 1){
+	          go_flag= 1;
+	         }
+         }
+
+        struct Node *head = dictionary -> head;
+        int number = 0;
+        while(head) {
+          ++number;
+          printf("%d%s%s%s\n", number, ". " head -> word, " ");
+	        printf("%s\n", head -> definition);
+
+	        head = head -> next;
     }
 
-    struct Node *head = dictionary -> head;
-    while(head){
-        printf("%s\n", head -> word);
-	printf("%s\n", head -> definition);
-
-	head = head -> next;
-    }
-    
 
     return 0;
 }
